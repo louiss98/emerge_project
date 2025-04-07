d2b45813694ad7398ab469e80e6c2274fc5ee308
Stefan Louis <stefanlouis98@gmail.com>
2025-04-07 15:49:30 -0400

    started implementing articulationview

--- isaac_sim/examples/hello_go2.py
+++ isaac_sim/examples/hello_go2.py
@@ -18,6 +18,7 @@ import omni.kit.app
 from omni.kit.async_engine import run_coroutine
 import queue
 import numpy as np
+from omni.isaac.dynamic_control import _dynamic_control
 
 
 
@@ -368,6 +369,12 @@ def combined_callback(step_size):
         }
         publish_queue.put(data_for_publish)
         print(f"Collected data from physics step -> step_size: {step_size} (every {n_publish_frequency} calls)")
+        dc = _dynamic_control.acquire_dynamic_control_interface()
+        articulation = dc.get_articulation(robot_prim_path)
+        # Call this each frame of simulation step if the state of the articulation is changing.
+        dc.wake_up_articulation(articulation)
+        joint_angles = [np.random.rand(9) * 2 - 1]
+        dc.set_articulation_dof_position_targets(articulation, joint_angles)
 
 
 def publish_worker():
